8f3100c0f36040e13a9152ba1168e09cc4311cb8
8ubble <lsdj1996@126.com>
2016-11-28 11:53:06 +0800

    layui->bootstrap
    8uuble

--- app/index.py
+++ app/index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def index(title=None):
     return render_template('index.html', title='河北比众')
 def header():
-	return render_template('header.html',)
+	return render_template('header.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
